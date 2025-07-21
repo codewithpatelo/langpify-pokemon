ecc54772de71a9859992066f43d52efccc5b5cb8
codewithpatelo <pj.patriciojulian@gmail.com>
2025-07-21 10:37:46 -0300

    refinement

--- app/domain/settings/routers.py
+++ app/domain/settings/routers.py
@@ -4,6 +4,9 @@ from app.presentation.routers import agent_routers
 
 def setup_routers(app: FastAPI) -> None:
     """
-    Configura la aplicación FastAPI con routers y middleware.
+    FastAPI Routers Set-Up
+    In productive environments we would have more services...
+    Such as a (MCP-Based) Tool Management Service, a Feedback and Eval Service, Memory Management Service, etc...
     """
     app.include_router(agent_routers.router)
+    
