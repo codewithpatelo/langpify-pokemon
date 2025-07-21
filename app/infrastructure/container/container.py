c5e5023060d72b0a83de0c0ea690ecf4e569d4ec
codewithpatelo <pj.patriciojulian@gmail.com>
2025-07-21 10:38:59 -0300

    refinement

--- app/infrastructure/container/container.py
+++ app/infrastructure/container/container.py
@@ -9,7 +9,7 @@ class Container(containers.DeclarativeContainer):
     Dependency Injection Container.
     This container is responsible for managing the dependencies of the application.
     It uses the `dependency_injector` library to provide singleton instances
-    of the MongoDB client and the conversation service. If in the future we need to change the database or
+    If in the future we need a database engine or
     add more services, we can do so by modifying this container.
     """
 
